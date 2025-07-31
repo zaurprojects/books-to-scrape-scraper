75969147d0e4b1421c21e0c6a9d22b66f2643aa1
zaurprojects <zaurxosrovov@gmail.com>
2025-07-31 17:54:04 +0400

    Update script.py

--- script.py
+++ script.py
@@ -1,15 +1,36 @@
 from bs4 import BeautifulSoup
 import requests
+import pandas as pd
 
 
-url = 'https://www.scrapethissite.com/pages/forms/'
+url = 'https://en.wikipedia.org/wiki/List_of_largest_companies_in_the_United_States_by_revenue'
 
 page = requests.get(url)
 
 soup = BeautifulSoup(page.text, 'html.parser')
 
-# result = soup.find_all('th')
+# There are two ways of doing this (I will use the second one):
+# result = soup.find('table', class_ = 'wikitable sortable')
+# result = soup.find_all('table')[1]
 
-result = soup.find('th').text.strip()
+table = soup.find_all('table')[1]
 
-print(result)
+world_titles = table.find_all('th')
+
+world_table_titles = [title.text.strip() for title in world_titles]
+
+# print(world_table_titles)
+
+df = pd.DataFrame(columns = world_table_titles)
+
+column_data = table.find_all('tr')
+
+for row in column_data[1:]:
+    row_data = row.find_all('td')
+    individual_row_data = [data.text.strip() for data in row_data]
+
+    length = len(df)
+    df.loc[length] = individual_row_data
+
+
+df.to_csv(r'C:\Users\User\PyCharmMiscProject\companies.csv')
